dbd29c9cae3e11ef396c574bd4a3baa851c1153f
Raven <152022920+Raven-002@users.noreply.github.com>
2025-05-10 20:05:26 +0300

    Fix media directory

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Starting RavenDB..."
+echo "Don't forget to set access permissions for for the chosen group"
+
 docker compose -f /opt/raven-compose/docker-compose.yml down
 HOSTNAME=$(hostname -i | awk '{print $1;}') \
 LISTENING_HOSTNAME=$(hostname -i | awk '{print $1;}') \
