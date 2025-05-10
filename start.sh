53461b9ec49060c419e750c80d81078e59a585d2
Raven <152022920+Raven-002@users.noreply.github.com>
2025-05-10 19:39:30 +0300

    Include media and move secrets to separate env file

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ LISTENING_HOSTNAME=$(hostname -i | awk '{print $1;}') \
 docker compose \
     -f /opt/raven-compose/docker-compose.yml \
     --env-file /opt/raven-compose/.env \
+    --env-file /opt/raven-compose/.env-secrets \
     up \
     -d
