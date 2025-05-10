44033b88cfeb587c01b7c661eb3ef9958ae48b98
Raven <152022920+Raven-002@users.noreply.github.com>
2025-05-10 21:27:55 +0300

    Add ai

--- start.sh
+++ start.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+set -e
+
 echo "Starting RavenDB..."
 echo "Don't forget to set access permissions for for the chosen group"
 
-docker compose -f /opt/raven-compose/docker-compose.yml down
+cat /opt/raven-compose/base.env /opt/raven-compose/secrets.env > /opt/raven-compose/.env
 HOSTNAME=$(hostname -i | awk '{print $1;}') \
 LISTENING_HOSTNAME=$(hostname -i | awk '{print $1;}') \
+echo "HOSTNAME=$HOSTNAME" >> /opt/raven-compose/.env
+echo "LISTENING_HOSTNAME=$LISTENING_HOSTNAME" >> /opt/raven-compose/.env
+
+docker compose -f /opt/raven-compose/docker-compose.yml down
 docker compose \
     -f /opt/raven-compose/docker-compose.yml \
     --env-file /opt/raven-compose/.env \
-    --env-file /opt/raven-compose/.env-secrets \
     up \
     -d
